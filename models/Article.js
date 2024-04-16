14eb447169bd7be898ef24e0e0125c5453f07251
SPorcelli4 <porcelli.silvio@gmail.com>
2024-04-16 10:26:38 +1000

    set up articles schema for mongodb

---
+++ models/Article.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const ArticleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: false
+    },
+});
+
+const Article = mongoose.model('Article', ArticleSchema);
+
+module.exports = Article;
