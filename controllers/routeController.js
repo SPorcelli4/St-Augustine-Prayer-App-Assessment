3ce06d0f08238e24cc01d60353f80595175e6960
SPorcelli4 <porcelli.silvio@gmail.com>
2024-05-03 17:07:54 +1000

    mvc model finished app looks very clean coding wise

---
+++ controllers/routeController.js
@@ -0,0 +1,52 @@
+const Article = require('../models/Article');
+
+
+module.exports.all_articles_get = async (req, res) => {
+    try {
+        const articles = await Article.find();
+        res.json(articles);
+    } catch (err) {
+        res.status(500).json({ msg: err.message });
+    }
+};
+
+module.exports.article_post = async (req, res) => {
+    const { title, body, tags } = req.body;
+    try {
+        const newArticle = new Article({ title, body, tags });
+        await newArticle.save();
+        res.status(201).json(newArticle);
+    } catch (err) {
+        res.status(400).json({ msg: err.message });
+    }
+};
+
+
+module.exports.random_article_get = async (req, res) => {
+    try {
+        const count = await Article.countDocuments();
+        const rand = Math.floor(Math.random() * count);
+        const randomArticle = await Article.findOne().skip(rand);
+        if (!randomArticle) {
+            return res.status(404).json({ msg: "No articles found" });
+        }
+        res.json(randomArticle);
+    } catch (err) {
+        res.status(500).json({ msg: err.message });
+    }
+};
+
+module.exports.random_article_by_tag_get = async (req, res) => {
+    const { tag } = req.query;
+    try {
+        const count = await Article.countDocuments({ tags: tag });
+        const rand = Math.floor(Math.random() * count);
+        const randomArticle = await Article.findOne({ tags: tag }).skip(rand);
+        if (!randomArticle) {
+            return res.status(404).json({ msg: "No articles found with the specified tag" });
+        }
+        res.json(randomArticle);
+    } catch (err) {
+        res.status(500).json({ msg: err.message });
+    }
+};
--- routes/articles.js
+++ routes/articles.js
@@ -1,59 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const Article = require('../models/Article');
+const routeController = require('../controllers/routeController');
 
-// POST a new article
-router.post('/', async (req, res) => {
-    const { title, body, tags } = req.body;
-    try {
-        const newArticle = new Article({ title, body, tags });
-        await newArticle.save();
-        res.status(201).json(newArticle);
-    } catch (err) {
-        res.status(400).json({ msg: err.message });
-    }
-});
 
 // GET all articles
-router.get('/', async (req, res) => {
-    try {
-        const articles = await Article.find();
-        res.json(articles);
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
-    }
-});
+router.get('/', routeController.all_articles_get)
 
+// POST a new article
+router.post('/', routeController.article_post)
 
 // Route to get a random article
-router.get('/random-article', async (req, res) => {
-    try {
-        const count = await Article.countDocuments();
-        const rand = Math.floor(Math.random() * count);
-        const randomArticle = await Article.findOne().skip(rand);
-        if (!randomArticle) {
-            return res.status(404).json({ msg: "No articles found" });
-        }
-        res.json(randomArticle);
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
-    }
-});
+router.get('/random-article', routeController.random_article_get)
 
+// GET a random article by tag
+router.get('/random-article-by-tag', routeController.random_article_by_tag_get)
 
-router.get('/random-article-by-tag', async (req, res) => {
-    const { tag } = req.query;
-    try {
-        const count = await Article.countDocuments({ tags: tag });
-        const rand = Math.floor(Math.random() * count);
-        const randomArticle = await Article.findOne({ tags: tag }).skip(rand);
-        if (!randomArticle) {
-            return res.status(404).json({ msg: "No articles found with the specified tag" });
-        }
-        res.json(randomArticle);
-    } catch (err) {
-        res.status(500).json({ msg: err.message });
-    }
-});
 
 module.exports = router;
