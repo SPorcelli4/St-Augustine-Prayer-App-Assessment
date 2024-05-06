62a9088a174843873d2b9df6b65d360540e5b6da
SPorcelli4 <porcelli.silvio@gmail.com>
2024-05-06 11:25:17 +1000

    Css imporvments + quality of life improvments

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 
+
 router.get('/signup', authController.signup_get);
 router.post('/signup', authController.signup_post);
 router.get('/login', authController.login_get);
--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const postController = require('../controllers/postController');
+const { requireAuth, checkUser } = require('../middleware/authMiddleware');
 
-
-router.get('/post', postController.post_get);
+router.get('/post', requireAuth, postController.post_get);
 router.post('/post', postController.post_post);
 router.get('/all-posts', postController.all_posts_get);
 
